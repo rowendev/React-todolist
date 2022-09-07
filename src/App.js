58c04e170005f69fc98ca38e2351eb1fe2870bb3
lxxch20 <lxxch20@gmail.com>
2022-09-07 20:04:28 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import NavBar from "./components/UI/NavBar";
+import CourseGoalList from "./components/CourseGoals/CourseGoalList/CourseGoalList";
+import CourseInput from "./components/CourseGoals/CourseInput/CourseInput";
+import "./App.css";
+import Footer from "./components/UI/Footer";
+
+const App = () => {
+  const [courseGoals, setCourseGoals] = useState([
+    { text: "Do more exercises!", id: "g1" },
+    { text: "Go to bed early!", id: "g2" },
+  ]);
+
+  const addGoalHandler = (enteredText) => {
+    setCourseGoals((prevGoals) => {
+      const updatedGoals = [...prevGoals];
+      updatedGoals.unshift({ text: enteredText, id: Math.random().toString() });
+      return updatedGoals;
+    });
+  };
+
+  const deleteItemHandler = (goalId) => {
+    setCourseGoals((prevGoals) => {
+      const updatedGoals = prevGoals.filter((goal) => goal.id !== goalId);
+      return updatedGoals;
+    });
+  };
+
+  let content = (
+    <p style={{ textAlign: "center" }}>No goals found. Maybe add one?</p>
+  );
+
+  if (courseGoals.length > 0) {
+    content = (
+      <CourseGoalList items={courseGoals} onDeleteItem={deleteItemHandler} />
+    );
+  }
+
+  return (
+    <div>
+      <NavBar />
+      <section id="goal-form">
+        <CourseInput onAddGoal={addGoalHandler} />
+      </section>
+      <section id="goals">
+        {content}
+        {/* {courseGoals.length > 0 && (
+          <CourseGoalList
+            items={courseGoals}
+            onDeleteItem={deleteItemHandler}
+          />
+        ) // <p style={{ textAlign: 'center' }}>No goals found. Maybe add one?</p>
+        } */}
+      </section>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/UI/NavBar.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./NavBar.module.css";
+
+const NavBar = () => {
+  return <div className={styles.div}>NavBar</div>;
+};
+
+export default NavBar;
